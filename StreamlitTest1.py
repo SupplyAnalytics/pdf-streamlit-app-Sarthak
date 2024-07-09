c290fa51172f387161e4cd8e47258f3dd0ec7d9a
unknown <sarthak.bhandari@bijnis.com>
2024-07-09 14:16:22 +0530

    New Commit

--- StreamlitTest1.py
+++ StreamlitTest1.py
@@ -1,3 +1,4 @@
+
 from __future__ import with_statement
 from AnalyticsClient import AnalyticsClient
 import pandas as pd
@@ -150,7 +151,6 @@ def generate_catalogue_pdf(Platform, subcategory, price_range, productcount, UTM
                         product_info = f'{product_name}<br/><br/><font size="18" color="red" style="text-decoration: underline; text-decoration-thickness: 2px;">CLICK </font><font size="18" color="black" style="text-decoration: underline; text-decoration-thickness: 2px;"> For More Info</font>'
                         hyperlink = f'<a href="{deeplink_url}" color="black">{product_info}</a>'
                         p = Paragraph(hyperlink, hyperlink_style)
-                        clicktext = 'CLICK For More Info'
 
                         if len(product_name) <= 20:
                             hyperlink_style.fontSize = 30
@@ -167,10 +167,7 @@ def generate_catalogue_pdf(Platform, subcategory, price_range, productcount, UTM
                         c.setStrokeColor('black')
                         c.setLineWidth(2)
                         c.setFillColor('black')
-                        if len(product_name) < len(clicktext):
-                            c.roundRect(x + (((max_image_width) / 2) - (pwidth / 2)) - 5,  y + 565,  300, 60, radius=10) 
-                        else:
-                            c.roundRect(x + (((max_image_width) / 2) - (pwidth / 2)) - 5, y + 565, pwidth + 40, 60, radius=10)
+                        c.roundRect(x + (((max_image_width) / 2) - (pwidth / 2)) - 5, y + 565, pwidth + 40, 60, radius=10)
                         p.wrapOn(c, max_image_width, max_image_height)
                         p.drawOn(c, x + (((max_image_width) / 2) - (pwidth / 2)), y + 590)
 
@@ -246,7 +243,7 @@ def generate_catalogue_pdf(Platform, subcategory, price_range, productcount, UTM
         if productcount is not None:
             if SellerName != "All":
                 df = df[(df['rankSeller'] <= productcount[1])]
-            if Platform == "BijnisExpress":
+            elif Platform == "BijnisExpress":
                 df = df[df['rankBijExp'] <= productcount[1]]
             elif Platform == "Production":
                 df = df[df['rankPP'] <= productcount[1]]
